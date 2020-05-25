f7fb66e33907c6135a2ae7eaa3f215ef50c10a43
Ivan Khorin <khorin.ivan.stefanovich@outlook.com>
2020-05-25 19:56:13 +0300

    Add task #1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTestTasks
 {
@@ -6,7 +8,18 @@ namespace LinqTestTasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var resultTask1 = Task1(_dataTask1);
         }
+
+        #region Task1
+        
+        static List<int> _dataTask1 = new List<int> { 3, 13, 12 , 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+
+        static List<int> Task1(List<int> data) =>
+            data.Where(x => x % 2 == 1)
+                .Distinct()
+                .ToList();
+
+        #endregion
     }
 }
