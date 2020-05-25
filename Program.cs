ca8c0ddffe34804a6d7ea4af35f1221359417277
Ivan Khorin <khorin.ivan.stefanovich@outlook.com>
2020-05-25 20:05:42 +0300

    Add task #2

--- Program.cs
+++ Program.cs
@@ -9,15 +9,28 @@ namespace LinqTestTasks
         static void Main(string[] args)
         {
             var resultTask1 = Task1(_dataTask1);
+
+            var resultTask2 = Task2(_dataTask2);
         }
 
-        #region Task1
+        #region Task #1
         
         static List<int> _dataTask1 = new List<int> { 3, 13, 12 , 157, 14, 77, 75, 13, 200, 156, 14, 21 };
 
         static List<int> Task1(List<int> data) =>
             data.Where(x => x % 2 == 1)
                 .Distinct()
+                .ToList();
+
+        #endregion
+
+        #region Task #2
+
+        static List<int> _dataTask2 = new List<int> { -12, -33, 134, 12, 1, -3, 99, 199, 5, 17, -121, 10};
+
+        static List<int> Task2(List<int> data) =>
+            data.Where(x => x > 0 && x.ToString().Length == 2)
+                .OrderBy(x => x)
                 .ToList();
 
         #endregion
